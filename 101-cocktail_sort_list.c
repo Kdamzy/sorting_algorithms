263812b100158a24ef221e1910a297d461cbed93
Kdamzy <kolajidedamilare@gmail.com>
2024-01-21 03:06:16 +0100

    a function that sorts a doubly linked list of integers in ascending order using the Cocktail shaker sort algorithm

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,104 +1,71 @@
 #include "sort.h"
-
-/**
- * swap_list - Swaps two elements in a doubly linked list.
- * @list: A double pointer to the head of the doubly linked list.
- * @low: The lower element to be swapped.
- * @high: The higher element to be swapped.
- */
-
-void swap_list(listint_t **list, listint_t *low, listint_t *high)
-{
-	low->next = high->next;
-	high->prev = low->prev;
-	low->prev = high;
-	high->next = low;
-
-	if (high->prev)
-		high->prev->next = high;
-	else
-		*list = high;
-
-	if (low->next)
-		low->next->prev = low;
-
-}
-
-/**
- * move - Moves the pointer in a doubly linked list.
- * @check: The current node in the list.
- * @pos: The position to move.
- * @dir: The direction to move (1 for forward, -1 for backward).
- * Return: The new position after moving.
- */
-
-listint_t *move(listint_t *check, int pos, int dir)
-{
-	int aii;
-
-	if (dir == 1)
-	{
-		for (aii = 0; aii < pos; aii++)
-			check = check->next;
-	}
-	else
-	{
-		while (check->next)
-			check = check->next;
-
-		for (aii = 0; aii < pos; aii++)
-			check = check->prev;
-	}
-	return (check);
-
-}
-
-/**
- * cocktail_sort_list - Sorts a doubly linked list
- * in ascending order using the Cocktail Sort algorithm.
- * @list: A double pointer to the head of the doubly linked list.
- */
-
 void cocktail_sort_list(listint_t **list)
 {
-	int swaps = 1;
-	int cycle = 0;
-	listint_t *check;
+	int swapped = 1;
+    listint_t *temp;
 
-	if (list == NULL || !(*list))
-		return;
+    if (list == NULL || *list == NULL)
+        return;
 
-	while (swaps != 0)
-	{
-		swaps = 0;
-		check = *list;
-		check = move(check, cycle, 1);
-		while (check->next)
-		{
-			if (check->n > check->next->n)
-			{
-				swap_list(list, check, check->next);
-				print_list(*list);
-				swaps++;
-				check = check->prev;
-			}
-			check = check->next;
-		}
-		if (swaps == 0)
-			return;
-		swaps = 0;
-		check = *list;
-		check = move(check, cycle, -1);
-		while (check->prev)
-		{
-			if (check->n < check->prev->n)
-			{
-				swap_list(list, check->prev, check);
-				print_list(*list);
-				swaps++;
-				check = check->next;
-			}
-			check = check->prev;
-		}
-	}
+    temp = *list;
+    while (swapped != 0)
+    {
+        swapped = 0;
+        while (temp->next != NULL)
+        {
+            if (temp->next->n < temp->n)
+            {
+                swap_nodes(list, temp, temp->next);
+                swapped = 1;
+                print_list(*list);
+            }
+            else
+            {
+                temp = temp->next;
+            }
+        }
+
+        if (swapped == 0)
+            break;
+
+        swapped = 0;
+        while (temp->prev != NULL)
+        {
+            if (temp->prev->n > temp->n)
+            {
+                swap_nodes(list, temp->prev, temp);
+                swapped = 1;
+                print_list(*list);
+            }
+            else
+            {
+                temp = temp->prev;
+            }
+        }
+    }
+}
+
+/**
+ * swap_nodes - Swaps two nodes in a doubly linked list.
+ * @list: Pointer to the head of the linked list.
+ * @node1: First node to swap.
+ * @node2: Second node to swap.
+ */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+    if (list == NULL || *list == NULL || node1 == NULL || node2 == NULL)
+        return;
+
+    if (node1->prev != NULL)
+        node1->prev->next = node2;
+    else
+        *list = node2;
+
+    if (node2->next != NULL)
+        node2->next->prev = node1;
+
+    node1->next = node2->next;
+    node2->prev = node1->prev;
+    node1->prev = node2;
+    node2->next = node1;
 }
--- sort.h
+++ sort.h
@@ -30,6 +30,11 @@ void quick_sort(int *array, size_t size);
 int partition(int *array, int first, int last, size_t size);
 void int_swap(int *m, int *n);
 void recuss_sort(int *array, int small, int big, size_t size);
+void selection_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+
 
 
 void print_array2(const int *array, size_t start, size_t end);
@@ -37,9 +42,6 @@ void print_array2(const int *array, size_t start, size_t end);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
